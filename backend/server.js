283f6f186835232642d2c790810fd642e2e66ea4
Ayesha <shaikayesha95155@gmail.com>
2022-11-30 00:46:52 +0530

    get chats api is created

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { chats } = require("./data/data");
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("api is running");
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('api/chat/:id', (req, res) => {
+    console.log(req);
+})
+
+app.listen(5000, console.log("server started on port 5000"));
